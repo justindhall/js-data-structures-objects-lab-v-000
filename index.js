806e91fb4264552c5a6fa92512a195632cfdd39d
Justin Hall <hall.justin.d@gmail.com>
2018-06-05 17:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value){
   
-  return Object.assign({ {}, driver, key: value});
+  return Object.assign({}, driver, { [key]: value });
   
 }
